fff65405b9ac3abc62c12516574d6b51b11675b4
Jose Daniel Garcia Camargo <112511914+JdGarcia667@users.noreply.github.com>
2024-08-16 13:26:20 -0600

    Prototipo escena de carga
    implantación de prototipo de escena de carga (falta hacerlo funcionar), para el cambio de escenas

--- Seleccion Natural/Principal/Main.gd
+++ Seleccion Natural/Principal/Main.gd
@@ -3,13 +3,15 @@ extends Node2D
 signal jugar
 signal salir
 
+
 const primer_nivel = preload("res://PrimerNivel/PrimerNivel.tscn")
 
 func _on_Salir_button_down():
 	emit_signal("salir")
 
 func _on_Jugar_button_down():
-	emit_signal("jugar")
+	#emit_signal("jugar")
+	#Global.Cargando(self, "res://PrimerNivel/PrimerNivel.tscn")
 	
 func _ready():
 	OS.center_window()
