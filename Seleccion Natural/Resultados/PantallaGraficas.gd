e11b154b1b997634b3e5ee6becd9e1b7c02b5fec
ceniceroscarlos0001 <carlos.ceniceros.mariaca@gmail.com>
2025-03-27 12:29:13 -0600

    Botón pasar gráficas
    Se agrego solo el botón para pasar de las gráficas

--- Seleccion Natural/Resultados/PantallaGraficas.gd
+++ Seleccion Natural/Resultados/PantallaGraficas.gd
@@ -1,12 +1,9 @@
 extends Panel
 
-var temporizador = 3
 var NivelActual = Conteo.NivelActual
 @onready var animationPlayer : AnimationPlayer = $AnimationPlayer
+		
 
-func _on_timer_timeout() -> void:
-	temporizador -= 1 #Vamos restando de 1 segundo el tiempo establecido (cuenta regresiva)
-	if temporizador == 0:
-		animationPlayer.play("fade_out")
-		Conteo.NivelActual += 1
-		LoadManager.load_scene("res://PrimerNivel/PrimerNivel.tscn")
+func _on_pasar_button_down() -> void:
+	Conteo.NivelActual += 1
+	LoadManager.load_scene("res://PrimerNivel/PrimerNivel.tscn")
